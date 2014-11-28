af500581ac611e016b97c92349db40ab95715f73
Matthieu Barba <matthieu.barba@igmors.u-psud.fr>
2014-11-28 11:50:18 +0100

    Proper quiz page name

--- quiz_def.php
+++ quiz_def.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
 <head>
-	<title>Anagrimes - Recherche avancée dans le Wiktionnaire</title>
+	<title>Quiz Anagrimes</title>
 	<meta http-equiv="Content-Type" content="text/HTML; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" media="screen" href="style/quiz.css" />
