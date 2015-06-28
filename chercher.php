ae8da28c8ab8f72916d644a5a27866fd87d221d3
Darkdadaah <darkdadaah@yahoo.fr>
2015-06-28 14:56:02 +0000

    rename recherche.css -> search.css

--- chercher.php
+++ chercher.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
 <head>
-	<title>Search Anagrimes</title>
+	<title>Anagrimes</title>
 	<meta http-equiv="Content-Type" content="text/HTML; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="shortcut icon" href="favicon.png" />
-	<link rel="stylesheet" type="text/css" media="screen" href="style/recherche.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="style/search.css" />
 	<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
 	<script type="text/javascript" src="js/search.js"></script>
 </head>
